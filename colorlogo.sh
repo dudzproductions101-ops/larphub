6024b08bff553ab1dd437e5fbe56bc09335d49fb
Beamy <128243705+beamyyl@users.noreply.github.com>
2026-08-23 18:53:22 +0300

    Colorlogo.sh

---
+++ colorlogo.sh
@@ -0,0 +1,21 @@
+GREEN='\033[1;32m'
+BROWN='\033[0;33m'
+RESET='\033[0m'
+printf "${GREEN}        .zJJ!.         
+     ..UUUYYYYU..      
+     :UUUYYYYYYY>.     
+    .UUUYYYYYYYYU.     
+    .]UYYYYYYYYUt\\zI.. 
+   .UUYYYYYYYYYYYYYYU. 
+  'YYYUYYYYYYYYYYYYUU. 
+  'YYYYYYYYYYYYYYYYYUU 
+  ..UYYYYYYYYYYYYYYYYYY
+  !YYYYYYYYYYYYYYYYYYUf
+  XYYYYYYYYYYYYYYYYUX. 
+ ..'UYYUUU'pC...... .. 
+${BROWN}         ..dk.         
+           dp          
+         ..bb..        
+         . hd'         
+           ad.         
+          ....         ${RESET}\n"
